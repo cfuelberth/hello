c16404da6e4352f0e6930923d6649b472564a0e4
Colin <cfuelberth@unomaha.edu>
2022-01-28 16:19:08 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Colin Fuelberth
+ *         cfuelberth@unomaha.edu
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Colin!\n");
+
+  return 0;
+}
